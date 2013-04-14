72e83570c3ea4c5476ce2289ca88bd954aa2c526
Gautier Nilhcem <nilhcem@gmail.com>
2013-04-14 17:46:02 +0200

    Add main code (while calls core methods and displays result)

--- src/main/java/com/nilhcem/mowitnow/App.java
+++ src/main/java/com/nilhcem/mowitnow/App.java
@@ -6,17 +6,24 @@ import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 
+import com.nilhcem.mowitnow.core.Mower;
+import com.nilhcem.mowitnow.core.instruction.MowerInstruction;
+import com.nilhcem.mowitnow.core.instruction.parser.InstructionsParser;
+
 /**
  * Starting point of the application.
- *
  * <p>
  * Opens file passed in args and calls the Mower interpreter.
  * </p>
  */
 public final class App {
 
+	private App() {
+	}
+
 	public static void main(String[] args) throws IOException {
 		List<String> instructions = App.getInstructionsFromArgFile(args);
+		runInstructions(instructions);
 	}
 
 	private static List<String> getInstructionsFromArgFile(String[] args) throws IOException {
@@ -26,5 +33,19 @@ public final class App {
 
 		File instructions = new File(args[0]);
 		return FileUtils.readLines(instructions, "UTF-8");
+	}
+
+	private static void runInstructions(List<String> data) {
+		InstructionsParser parser = new InstructionsParser(data);
+
+		Mower mower = parser.getNextMower();
+		while (mower != null) {
+			List<MowerInstruction> instrs = parser.getInstructionsForMower(mower);
+			for (MowerInstruction instr : instrs) {
+				mower.process(instr);
+			}
+			System.out.println(mower.getPosition());
+			mower = parser.getNextMower();
+		}
 	}
 }
